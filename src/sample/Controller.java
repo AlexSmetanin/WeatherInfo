bc7afa2d7bb845d6a48f5be2547f0271dab16b86
AlexSmetanin <AlexSmetanin72@gmail.com>
2022-03-10 14:36:05 +0200

    Font color changed, Enter button reaction added.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -11,6 +11,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import org.json.JSONObject;
 
+import javax.swing.*;
+
 public class Controller {
 
     @FXML
@@ -45,25 +47,32 @@ public class Controller {
     void initialize() {
         // При натисненні на кнопку
         getData.setOnAction(event -> {
-            // Отримуємо дані з текстового поля
-            String userCity = city.getText().trim();
-            if (!userCity.equals("")) { // якщо дані не пусті
-                // Отримуємо дані про погоду з сайту openweathermap.org
-                String output = getUrlContent("http://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&APPID=5d0788ab0be4ac1f063b81f9be16785c&units=metric");
-
-                if (!output.isEmpty()) { // Нема помилки і таке місто існує
-                    JSONObject object = new JSONObject(output);
-                    // Обробляємо JSON і встановлюємо дані в текстові написи
-                    temp_info.setText("Температура: " + object.getJSONObject("main").getDouble("temp"));
-                    temp_feels.setText("По-вiдчуттям: " + object.getJSONObject("main").getDouble("feels_like"));
-                    temp_max.setText("Максимум: " + object.getJSONObject("main").getDouble("temp_max"));
-                    temp_min.setText("Мiнiмум: " + object.getJSONObject("main").getDouble("temp_min"));
-                    pressure.setText("Тиск: " + object.getJSONObject("main").getDouble("pressure"));
-                }
-            }
+            someMethod();
+        });
+        // При натисненні Enter в полi введення
+        city.setOnAction(event -> {
+            someMethod();
         });
     }
 
+    private void someMethod() {
+        // Отримуємо дані з текстового поля
+        String userCity = city.getText().trim();
+        if (!userCity.equals("")) { // якщо дані не пусті
+            // Отримуємо дані про погоду з сайту openweathermap.org
+            String output = getUrlContent("http://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&APPID=5d0788ab0be4ac1f063b81f9be16785c&units=metric");
+
+            if (!output.isEmpty()) { // Нема помилки і таке місто існує
+                JSONObject object = new JSONObject(output);
+                // Обробляємо JSON і встановлюємо дані в текстові написи
+                temp_info.setText("Температура: " + object.getJSONObject("main").getDouble("temp"));
+                temp_feels.setText("По-вiдчуттям: " + object.getJSONObject("main").getDouble("feels_like"));
+                temp_max.setText("Максимум: " + object.getJSONObject("main").getDouble("temp_max"));
+                temp_min.setText("Мiнiмум: " + object.getJSONObject("main").getDouble("temp_min"));
+                pressure.setText("Тиск: " + object.getJSONObject("main").getDouble("pressure"));
+            }
+        }
+    }
     // Обробка URL адресу і отримання даних з нього
     private static String getUrlContent(String urlAddress) {
         StringBuffer content = new StringBuffer();
@@ -79,7 +88,7 @@ public class Controller {
             }
             bufferedReader.close();
         } catch (Exception e) {
-            System.out.println("Таке місто не було знайдено!"); // TODO замінити на вікно-месседж
+            JOptionPane.showMessageDialog(null, "Таке місто не було знайдено!", "Помилка", JOptionPane.ERROR_MESSAGE);
         }
         return content.toString();
     }
